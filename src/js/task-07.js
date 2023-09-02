5e924a14c1416e3764fffaec5564b7b96ed3fae3
ZbyszekZebrowski <137501903+ZbyszekZebrowski@users.noreply.github.com>
2023-09-02 14:48:19 +0300

    asss

--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const fontSizeControl = document.getElementById("font-size-control");
-const textElement = document.getElementById("text");
-
-fontSizeControl.addEventListener("input", () => {
-  textElement.style.fontSize = `${fontSizeControl.value}px`;
-});
