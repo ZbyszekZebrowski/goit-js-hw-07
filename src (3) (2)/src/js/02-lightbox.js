d32695b963eb317c409e230651161bf0c2bc80f7
ZbyszekZebrowski <137501903+ZbyszekZebrowski@users.noreply.github.com>
2023-09-02 16:24:31 +0300

    simplelight box

--- src (3) (2)/src/js/02-lightbox.js
+++ src (3) (2)/src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', function () {
+  const gallery = document.querySelector('.gallery');
+
+  
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`)
+    .join('');
+
+  gallery.innerHTML = galleryMarkup;
+
+
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
